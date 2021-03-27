758883d0f02e8635826af3ef78c0c9f3f06c0a2e
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-27 14:11:16 +0100

    Create conWhile.java

---
+++ conWhile.java
@@ -0,0 +1,11 @@
+ public class conWhile {
+    public static void main(String[] args) {
+        System.out.println("Numeros del 100 al 1 con bucle While: ");
+         int i=100;
+         while(i>=1)
+         {
+                   System.out.println(i);
+                   i--;
+         }
+    }
+}
